3ee2bb26ccf700c96007a5bb59f0ffbae2d16bba
JJCG6842 <cisnerosjuan6842@gmail.com>
2025-09-28 09:19:20 -0500

    backend

---
+++ src/api/categoria/categoria.controller.ts
@@ -0,0 +1,35 @@
+import { Controller , Get, Post, Put, Delete, Param, Body, ParseIntPipe} from '@nestjs/common';
+import { CategoriaService } from './categoria.service';
+
+@Controller('categoria')
+export class CategoriaController {
+    constructor(private categoriaService: CategoriaService){}
+
+    @Get()
+    getAll(){
+        return this.categoriaService.findAll();
+    }
+
+    @Get(':id')
+    getOne(@Param('id', ParseIntPipe) id:number){
+        return this.categoriaService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() body: {name: string; description: string}){
+        return this.categoriaService.create(body);
+    }
+
+    @Put(':id')
+    update(
+        @Param('id', ParseIntPipe) id:number,
+        @Body() body: {name?: string; description?: string },
+    ) {
+        return this.categoriaService.update(id, body);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id:number) {
+        return this.categoriaService.delete(id);
+    }
+}
---
+++ src/api/categoria/categoria.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class CategoriaService {
+    private prisma = new PrismaClient();
+
+    async findAll() {
+        return this.prisma.category.findMany({
+            include: {products: true},
+        });
+    }
+
+    async findOne(id: number){
+        const category = await this.prisma.category.findUnique({
+            where: {id},
+            include: {products: true},
+        });
+        if (!category) throw new NotFoundException('category not found');
+        return category;
+    }
+
+    async create(data: {name :string; description: string}){
+        return this.prisma.category.create({data});
+    }
+
+    async update(id: number, data: { name?: string; descrption?: string}){
+        await this.findOne(id);
+        return this.prisma.category.update({
+            where: {id},
+            data,
+        });
+    }
+
+    async delete(id:number){
+        await this.findOne(id);
+        return this.prisma.category.delete({where: {id}})
+    }
+}
